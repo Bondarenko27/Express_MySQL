b5d9c86e2b3a94cf44a531f1f6a75b4cda939c73
Bondarenko27 <bondarenkokristina27@gmail.com>
2021-11-09 12:56:07 -0500

    import database into app

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const db = require("./database");
+app.get("/",(req,res,next)=>{
+    db.execute("Select*From users"). then(([rows, metaData])=>{
+        const data={...rows[0]};
+        const parsedData = JSON.stringify(data);
+        res.json(parsedData);
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+});
 app.listen (3000,()=>{
 console.log("server is up and running on port 3000");
 });

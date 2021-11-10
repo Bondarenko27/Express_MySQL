4c051aad2e90dd1bb1ace4817474977914c5b9a3
Bondarenko27 <bondarenkokristina27@gmail.com>
2021-11-10 09:33:12 -0500

    add html

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const path = require ('path');
 const app = express();
 const db = require("./database");
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,'/index.html'));
+});
 app.get("/",(req,res,next)=>{
     db.execute("Select usersName From users"). then(([rows, metaData])=>{
         const data={rows};
@@ -11,6 +15,7 @@ app.get("/",(req,res,next)=>{
         console.log(err);
     });
 });
+
 app.listen (3000,()=>{
 console.log("server is up and running on port 3000");
 });

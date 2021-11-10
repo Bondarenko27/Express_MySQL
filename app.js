7d9952443bd6547eb75b917be24992c9b09330f8
Bondarenko27 <bondarenkokristina27@gmail.com>
2021-11-10 10:17:26 -0500

    removed get request

--- app.js
+++ app.js
@@ -5,17 +5,6 @@ const db = require("./database");
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'/index.html'));
 });
-app.get("/",(req,res,next)=>{
-    db.execute("Select usersName From users"). then(([rows, metaData])=>{
-        const data={rows};
-        const parsedData = JSON.stringify(data);
-        res.json(parsedData);
-    })
-    .catch((err)=>{
-        console.log(err);
-    });
-});
-
 app.listen (3000,()=>{
 console.log("server is up and running on port 3000");
 });

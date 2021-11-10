9a17625f2e1eaac42671eaa1c71ebeb802619f3e
Bondarenko27 <bondarenkokristina27@gmail.com>
2021-11-10 10:39:57 -0500

    removed routes path

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const path = require ('path');
 const app = express();
 const db = require("./database");
+var usersRouter = require ("./users");
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,'/index.html'));
 });
+app.use('/',usersRouter);
 app.listen (3000,()=>{
 console.log("server is up and running on port 3000");
 });
